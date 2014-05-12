68a705e9e906ff3f8f3a7d5a51fe4c11b4f6c9d5
pthelusma <pthelusma@gmail.com>
2014-05-11 21:09:34 -0400

    Initial commit

---
+++ DragAndDrop/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DragAndDrop.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            string contentPath = "~/Content/upload/";
+            string something = Server.MapPath(contentPath);
+
+            string[] fileNames = Directory.GetFiles(something)
+                .Select(path => contentPath + Path.GetFileName(path))
+                .ToArray();
+
+            return View(fileNames);
+        }
+
+        [HttpPost]
+        public ActionResult UploadImage(HttpPostedFileBase file)
+        {
+            UploadFile(file);
+            return Content(file.FileName);
+        }
+
+        public void UploadFile(HttpPostedFileBase file)
+        {
+            System.IO.File.WriteAllBytes(Server.MapPath("~/Content/upload/" + file.FileName), ReadData(file.InputStream));
+        }
+
+        private byte[] ReadData(Stream stream)
+        {
+            var buffer = new byte[16 * 1024];
+ 
+            using (var ms = new MemoryStream())
+            {
+                int read;
+
+                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+ 
+                return ms.ToArray();
+            }
+        }
+
+    }
+}
---
+++ DragAndDrop/Scripts/script.js
@@ -0,0 +1,94 @@
+﻿/// <reference path="jquery-1.8.3.js" />
+
+$(function () {
+    var dropbox = $('#dropbox'),
+        imageGallery = $('#imgGallery'),
+		message = $('.message', dropbox),
+        fileIndex = 0;
+
+    dropbox.filedrop({
+        // The name of the $_FILES entry:
+        paramname: 'file',
+        maxfiles: 5,
+        maxfilesize: 2, // in mb
+        url: '/home/UploadImage',
+        uploadFinished: function (i, file, response) {
+            $.data(file).addClass('done');
+            // response code here
+        },
+        error: function (err, file) {
+            switch (err) {
+                case 'BrowserNotSupported':
+                    showMessage('Your browser does not support HTML5 file uploads!');
+                    break;
+                case 'TooManyFiles':
+                    alert('Too many files! Please select 5 at most!');
+                    break;
+                case 'FileTooLarge':
+                    alert(file.name + ' is too large! Please upload files up to 2mb.');
+                    break;
+                default:
+                    break;
+            }
+        },
+        // Called before each upload is started
+        beforeEach: function (file) {
+            if (!file.type.match(/^image\//)) {
+                alert('Only images are allowed!');
+
+                // Returning false will cause the
+                // file to be rejected
+                return false;
+            }
+        },
+        uploadStarted: function (i, file, len) {
+            fileIndex++;
+            createImage(file);
+        },
+        progressUpdated: function (i, file, progress) {
+            $.data(file).find('.progress').width(progress);
+        }
+    });
+
+    var template =
+        '<div class="preview">' +
+            '<span class="imageHolder">' +
+                '<img />' +
+                '<span class="uploaded"></span>' +
+            '</span>' +
+            '<div class="progressHolder">' +
+                '<div class="progress"></div>' +
+            '</div>' +
+        '</div>';
+
+    function createImage(file) {
+
+        var preview = $(template),
+            image = $('img', preview);
+
+        var reader = new window.FileReader();
+
+        image.width = 100;
+        image.height = 100;
+
+        reader.onload = function (e) {
+            image.attr('src', e.target.result);
+        };
+
+        // Reading the file as a DataURL. When finished,
+        // this will trigger the onload function above:
+        reader.readAsDataURL(file);
+
+        //message.hide();
+        preview.appendTo(imageGallery);
+        //$('.preview').attr('data-index', fileIndex);
+        // Associating a preview container
+        // with the file, using jQuery's $.data():
+
+        $.data(file, preview);
+    }
+
+    function showMessage(msg) {
+        message.html(msg);
+    }
+});
